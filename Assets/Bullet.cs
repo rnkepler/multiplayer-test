10cba82c3620350f932f8a85dbde1dfa93f210e3
rnkepler <rnkepler@gmail.com>
2017-04-08 18:59:23 -0400

    Added player health

---
+++ Assets/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMove>();
+        if (hitPlayer != null)
+        {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Combat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int health = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            health = 0;
+            Debug.Log("Dead!");
+        }
+    }
+}
