5827b86a632a312869b0464da95e1a0342c27200
rnkepler <rnkepler@gmail.com>
2017-04-08 17:01:01 -0400

    Support for multiple clients.  Objects update on all clients

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,9 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isLocalPlayer)
+            return;
+
         var x = Input.GetAxis("Horizontal") * 0.1f;
         var z = Input.GetAxis("Vertical") * 0.1f;
 
