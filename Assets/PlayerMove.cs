7836319cd080d308200e4c21c5ec48746c491945
rnkepler <rnkepler@gmail.com>
2017-04-08 17:18:33 -0400

    Added bullets to client, not network

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour {
+    public GameObject bulletPrefab;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 
@@ -25,5 +26,25 @@ public class PlayerMove : NetworkBehaviour {
         var z = Input.GetAxis("Vertical") * 0.1f;
 
         transform.Translate(x, 0, z);
-	}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
 }
