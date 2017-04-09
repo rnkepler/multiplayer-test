2532ac41242fb06f80b0e4909ac9d5ccf03d3b15
rnkepler <rnkepler@gmail.com>
2017-04-09 12:36:34 -0400

    Added player death and respawn

--- Assets/Combat.cs
+++ Assets/Combat.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Combat : MonoBehaviour
+public class Combat : NetworkBehaviour
 {
     public const int maxHealth = 100;
+
+    [SyncVar]
     public int health = maxHealth;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+            return;
+
         health -= amount;
         if (health <= 0)
         {
-            health = 0;
-            Debug.Log("Dead!");
+            health = maxHealth;
+
+            // called on the server, will be invoked on the clients
+            RpcRespawn();
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
         }
     }
 }
